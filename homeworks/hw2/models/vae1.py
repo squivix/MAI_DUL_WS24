495068f813f978a5080233b03c87f8622fdd0071
devintheamateurdevin <devintheamateurdevin@gmail.com>
2024-11-27 06:59:51 +0100

    fixes loss function in hw2 q1

--- homeworks/hw2/models/vae1.py
+++ homeworks/hw2/models/vae1.py
@@ -1,10 +1,9 @@
 import torch
 from torch import nn
+from torch.distributions import Normal, MultivariateNormal
 
-from torch.nn import functional as F
 
-
-class VAEModel(nn.Module):
+class VAEModel1(nn.Module):
     def __init__(self, input_size, latent_size, hidden_units, regularization_weight, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.input_size = input_size
@@ -32,14 +31,17 @@ class VAEModel(nn.Module):
         decoder_logits = self.decoder.forward(z)
         mu_x_fake = decoder_logits[:, :self.input_size]
         log_var_x_fake = decoder_logits[:, self.input_size:]
-        sigma_x_fake = torch.exp(0.5 * log_var_x_fake)
-        x_fake = sigma_x_fake * torch.randn_like(sigma_x_fake) + mu_x_fake
-        return x_fake, mu_z, log_var_z
+
+        # x_fake = sigma_x_fake * torch.randn_like(sigma_x_fake) + mu_x_fake
+        return mu_x_fake, log_var_x_fake, mu_z, log_var_z
 
     def loss_function(self, x_true, model_output):
-        x_fake, mu, log_var = model_output
-        reconstruction_loss = F.mse_loss(x_fake, x_true)
-        regularization_term = torch.mean(0.5 * torch.sum(mu ** 2 + log_var.exp() - log_var - 1, dim=1), dim=0)
+        mu_x_fake, log_var_x_fake, mu_z, log_var_z = model_output
+        sigma_x_fake = torch.exp(0.5 * log_var_x_fake)
+        x_dist = MultivariateNormal(mu_x_fake, torch.diag_embed(sigma_x_fake))
+        reconstruction_loss = -torch.mean(x_dist.log_prob(x_true), dim=0)
+        MultivariateNormal(mu_x_fake, torch.diag_embed(sigma_x_fake))
+        regularization_term = torch.mean(0.5 * torch.sum(mu_z ** 2 + log_var_z.exp() - log_var_z - 1, dim=1), dim=0)
         loss = (1 - self.beta) * reconstruction_loss + self.beta * regularization_term
         return loss, reconstruction_loss, regularization_term
 
